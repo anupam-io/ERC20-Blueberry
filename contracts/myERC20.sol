4f56c5a33eaf33629852c74127b19dc2f7b958ce
akcgjc007 <akcgjc007@gmail.com>
2021-03-09 22:57:15 +0530

    added minting and burning.

--- contracts/myERC20.sol
+++ contracts/myERC20.sol
@@ -4,13 +4,23 @@ pragma solidity >=0.6.2 <0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract myERC20 is ERC20{
+    address mainMinter;
     constructor(
         string memory _name, 
-        string memory _symbol
+        string memory _symbol,
+        address _mainMinter
     ) ERC20(_name, _symbol){
+        mainMinter = _mainMinter;
     }
 
-    function mint(address to, uint amount) external {
-        _mint(to, amount);
+    function mint(address account, uint amount) external{
+        require(msg.sender == mainMinter);
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint amount)external{
+        require(msg.sender == mainMinter);
+        require(account == msg.sender);
+        _burn(account, amount);
     }
 }
--- test/mytest.js
+++ test/mytest.js
@@ -1,4 +1,4 @@
-const DAI = artifacts.require("DAI");
+const myERC20 = artifacts.require("myERC20");
 
 const inWei = 10**18;
 
@@ -6,16 +6,26 @@ contract('RFT', async addresses => {
 	const [admin, buyer1, buyer2, _] = addresses;
 
 	it.only('ERC20 is working.', async () => {
-		const dai = await DAI.new("Blueberry", "BBR");
-		await dai.name().then(data=>console.log("Name: ", data));
-		await dai.symbol().then(data=>console.log("Symbol: ", data));
+		const con = await myERC20.new("Blueberry", "BBR", admin);
+		await con.name().then(data=>console.log("Name: ", data));
+		await con.symbol().then(data=>console.log("Symbol: ", data));
 		
-		await dai.mint(buyer1, (10*inWei).toString());
-		await dai.mint(buyer2, (10*inWei).toString());
-		
-		await dai.balanceOf(buyer1).then(data=>console.log("Buyer1: ", (data/inWei).toString()))
-		await dai.balanceOf(buyer2).then(data=>console.log("Buyer2: ", (data/inWei).toString()))
+        const initAmount = (100*inWei).toString();
+        const sendAmount = (10*inWei).toString();
+		const burnAmount = (80*inWei).toString();
 
-		await dai.totalSupply().then(data=>console.log("Supply: ", (data/inWei).toString()));
-	});
+        await con.mint(admin, initAmount, {from: admin});
+		await con.balanceOf(admin).then(data=>console.log("Admin: ", (data/inWei).toString()))
+        await con.totalSupply().then(data=>console.log("Total supply: ", (data/inWei).toString()));		
+        
+        await con.transfer(buyer1, sendAmount, {from: admin});
+        await con.transfer(buyer2, sendAmount, {from: admin});
+		await con.balanceOf(admin).then(data=>console.log("Admin: ", (data/inWei).toString()))
+		await con.balanceOf(buyer1).then(data=>console.log("Buyer1: ", (data/inWei).toString()))
+		await con.balanceOf(buyer2).then(data=>console.log("Buyer2: ", (data/inWei).toString()))
+		
+        await con.burn(admin, burnAmount, {from: admin});
+		await con.balanceOf(admin).then(data=>console.log("Admin: ", (data/inWei).toString()))
+		await con.totalSupply().then(data=>console.log("Total supply: ", (data/inWei).toString()));
+    });
 });
